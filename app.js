3d65204969bbdf2c41a194cfd94a421ae138e43f
ISMAILAWA <ismailawa.aliyu@gmail.com>
2020-05-24 20:45:52 +0100

    added frontend

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+});
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
